bb263f2dd951c6253aca102c8d03b73151263e56
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-24 19:35:41 -0600

    Create Sentenciawhile.java

---
+++ Sentenciawhile.java
@@ -0,0 +1,12 @@
+public class Sentenciawhile {
+    public static void main(String[] args) {
+        int numero = 1;
+        //La sentencia while sirve para hacer siclos hasta que se cumpla la condicion
+        while (numero<=1000){
+        System.out.println(numero);
+        numero ++;
+
+        }
+    }
+    
+}
